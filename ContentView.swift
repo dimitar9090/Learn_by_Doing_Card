082900251b4f5bf603db09949d90b57dcb8edcab
Dimitar Anguelov   <113285865+dimitar9090@users.noreply.github.com>
2024-01-26 01:36:32 +0200

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -11,8 +11,6 @@ struct ContentView: View {
   
   var cards: [Card] = cardData
   
-  // MARK: - CONTENT
-  
   var body: some View {
     ScrollView(.horizontal, showsIndicators: false) {
       HStack(alignment: .center, spacing: 20) {
@@ -24,8 +22,6 @@ struct ContentView: View {
     }
   }
 }
-
-// MARK: - PREVIEW
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
